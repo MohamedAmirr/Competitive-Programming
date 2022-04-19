ed3f9fea18772c9871a79753dfccd58d300883e4
Mohamed Amir <90771670+MohamedAmirr@users.noreply.github.com>
2022-04-19 16:16:22 +0200

    Add files via upload

---
+++ Algorithms and Techniques/Ceiling.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define besmellah ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+const ll INF = 1e9 + 7;
+const ll MOD = 1e9 + 7;
+#define M_PI 3.14159265358979323846
+#define endl '\n'
+
+int f_ceil(int a,int b){
+    return (a/b)+((a%b)!=0);
+}
+
+int s_ciel(int a,int b){
+    return ((a+b)-1)/b;
+}
+
+int main() {
+    besmellah
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+//    freopen("closing.out", "w", stdout);
+#endif
+
+}
