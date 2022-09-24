88d779fa710342d0d106190b472cb8bc8d7420f5
Mohamed Amir <90771670+MohamedAmirr@users.noreply.github.com>
2022-09-24 18:28:55 +0200

    Update DSU.cpp

--- Algorithms and Techniques/DSU.cpp
+++ Algorithms and Techniques/DSU.cpp
@@ -1,3 +1,6 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+
 #include <bits/stdc++.h>
 
 #define besmellah ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -6,56 +9,83 @@ typedef long long ll;
 typedef long double ld;
 typedef unsigned long long ull;
 const ll INF = 1e9 + 7;
+
+const int OO = 0x3f3f3f3f;
+const ll LOO = 0x3f3f3f3f3f3f3f3f;
 const ll MOD = 1e9 + 7;
 const ll N = 1e5;
+const double EPS = 1e-6;
 #define M_PI 3.14159265358979323846
+#define all(a)  a.begin(),a.end()
 #define endl '\n'
+
+int lg(int n) { return 31 - __builtin_clz(n); }
+
 const int dx[] = {+0, +0, +1, -1, +1, -1, +1, -1};
 const int dy[] = {+1, -1, +0, +0, +1, -1, -1, +1};
-const int knightDx[] = {+1, -1, +2, +2, -2, -2, +1, -1};
-const int knightDy[] = {+2, +2, +1, -1, +1, -1, -2, -2};
+int knightDx[] = {+1, -1, +2, +2, -2, -2, +1, -1};
+int knightDy[] = {+2, +2, +1, -1, +1, -1, -2, -2};
+//#define int long long
 
-int arr[N + 5];
+struct Node {
+    int id;
+    Node *par = this;
+    int sz = 1;
 
-int findRoot(int i) {
-    if (arr[i] != i) {
-        return arr[i] = findRoot(arr[i]);
-    } else {
-        return i;
-    }
+    Node(int id) : id(id) {};
+};
+
+Node *findPar(Node *node) {
+    if (node == node->par)
+        return node;
+    return node->par = findPar(node->par);
 }
 
-void DSU(int a, int b) {
-    a = findRoot(a);
-    b = findRoot(b);
-    if (a != b){
-        //arr[b]+=arr[a] : lw nta btsht8l bl tre2a bta3t code ncode w hwa by3ml initialize kolo b -1
-        arr[b] = a;
+void combine(Node *a, Node *b) {
+    a = findPar(a);
+    b = findPar(b);
+    if (a == b)return;
+    if (a->sz < b->sz) {
+        swap(a, b);
     }
-
-    else
-        return;
+    b->par = a;
 }
 
-int main() {
+signed main() {
     besmellah
-#ifndef ONLINE_JUDGE
+//Sky is the limit ❤
+#ifdef CLION
     freopen("input.txt", "r", stdin);
-    //    freopen("closing.out", "w", stdout);
+    freopen("output.txt", "w", stdout);
+#else
+    //freopen("", "r", stdin);
 #endif
-    int n, m;
-    cin >> n >> m;
-    for (int i = 0; i <= n + 5; ++i) {
-        arr[i] = i;
+    int T = 1;
+    //cin >> T;
+//remember data type long long or int
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+        vector<Node *> nodes(n);
+        for (int i = 0; i < n; ++i) {
+            nodes[i] = new Node(i + 1);
+        }
+        while (m--) {
+            string s;
+            cin >> s;
+            int a, b;
+            cin >> a >> b;
+            a--, b--;
+            if (s == "union")combine(nodes[a], nodes[b]);
+            else if(s == "get"){
+                a = findPar(nodes[a])->id;
+                b = findPar(nodes[b])->id;
+                if (a == b) {
+                    cout << "YES\n";
+                } else cout << "NO\n";
+            }
+        }
     }
-    while (m--) {
-        string s;
-        int u, v;
-        cin >> s >> u >> v;
-        if (s == "union") DSU(u, v);
-        else if (arr[u] != arr[v])
-            findRoot(u) == findRoot(v) ? cout << "YES\n" : cout << "NO\n";
-        else if (arr[u] == arr[v])
-            cout << "YES\n";
-    }
+//remember data type long long or int
+
 }
