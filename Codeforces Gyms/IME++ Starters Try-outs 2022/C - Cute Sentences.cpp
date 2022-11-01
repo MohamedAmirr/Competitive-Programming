4001af249c1fdae787e63bb0e1c53c90484413f5
Mohamed Amir <90771670+MohamedAmirr@users.noreply.github.com>
2022-11-01 20:17:54 +0200

    Create C - Cute Sentences.cpp

---
+++ Codeforces Gyms/IME++ Starters Try-outs 2022/C - Cute Sentences.cpp
@@ -0,0 +1,68 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+
+#define besmellah ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+const ll INF = 1e9 + 7;
+
+const int OO = 0x3f3f3f3f;
+const ll LOO = 0x3f3f3f3f3f3f3f3f;
+const ll MOD = 1e9 + 7;
+const ll N = 1e5;
+const double EPS = 1e-6;
+#define M_PI 3.14159265358979323846
+#define all(a)  a.begin(),a.end()
+#define endl '\n'
+
+int lg(int n) { return 31 - __builtin_clz(n); }
+
+const int dx[] = {+0, +0, +1, -1, +1, -1, +1, -1};
+const int dy[] = {+1, -1, +0, +0, +1, -1, -1, +1};
+int knightDx[] = {+1, -1, +2, +2, -2, -2, +1, -1};
+int knightDy[] = {+2, +2, +1, -1, +1, -1, -2, -2};
+//#define int long long
+
+
+signed main() {
+    besmellah
+//Sky is the limit ❤
+#ifdef CLION
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    //freopen("", "r", stdin);
+#endif
+    int T = 1;
+    //cin >> T;
+//remember data type long long or int
+    while (T--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        reverse(all(s));
+        s.pop_back();
+        bool ans = 1;
+        for (int i = 0; i < n - 1; ++i) {
+            string str;
+            cin >> str;
+            if (s.back() == str.front()) {
+                s.pop_back();
+            } else {
+                ans = 0;
+            }
+        }
+        if (ans and s.empty()) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+//remember data type long long or int
+
+}
