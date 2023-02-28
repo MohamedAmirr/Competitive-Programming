0263aca0796ae5b0fcd2928fb44cd567f6c946f2
Mohamed Amir <m.elamir027@gmail.com>
2023-02-28 22:58:11 +0200

    Create G - Grand Meeting.cpp

---
+++ Codeforces Gyms/VII MaratonUSP Freshman Contest/G - Grand Meeting.cpp
@@ -0,0 +1,67 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+
+#define besmellah ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+const int OO = 0x3f3f3f3f;
+const ll LOO = 0x3f3f3f3f3f3f3f3f;
+const ll MOD = 1e9 + 7;
+const ll N = 1e5;
+const double EPS = 1e-6;
+#define M_PI 3.14159265358979323846
+#define all(a)  a.begin(),a.end()
+#define endl '\n'
+
+int lg(int n) { return 31 - __builtin_clz(n); }
+
+const int dx[] = {+0, +0, +1, -1, +1, -1, +1, -1};
+const int dy[] = {+1, -1, +0, +0, +1, -1, -1, +1};
+int knightDx[] = {+1, -1, +2, +2, -2, -2, +1, -1};
+int knightDy[] = {+2, +2, +1, -1, +1, -1, -2, -2};
+//#define int long long
+
+
+
+
+signed main() {
+    besmellah
+//Sky is the limit ❤
+#ifdef CLION
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    //freopen("", "r", stdin);
+#endif
+    int T = 1;
+//    cin >> T;
+//remember data type long long or int
+    while (T--) {
+        int n;
+        cin >> n;
+        map<string, int> mp;
+        for (int i = 0; i < n; ++i) {
+            string s;
+            cin >> s;
+            mp[s] = i;
+        }
+        string a, b;
+        cin >> a >> b;
+        if (mp[b] < mp[a])swap(mp[b], mp[a]);
+        ll sum = 0;
+        while (mp[a] < mp[b]) {
+            mp[a]++;
+            mp[b]--;
+            sum++;
+        }
+        cout << sum << endl;
+
+    }
+//remember data type long long or int
+
+}
