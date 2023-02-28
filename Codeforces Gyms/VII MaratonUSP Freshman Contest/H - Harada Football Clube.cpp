a4e096f36498957a1100b081a15f348a0a32ec08
Mohamed Amir <m.elamir027@gmail.com>
2023-02-28 22:59:10 +0200

    Create H - Harada Football Clube.cpp

---
+++ Codeforces Gyms/VII MaratonUSP Freshman Contest/H - Harada Football Clube.cpp
@@ -0,0 +1,57 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+
+#include <bits/stdc++.h>
+
+#define besmellah ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+const int OO = 0x3f3f3f3f;
+const ll LOO = 0x3f3f3f3f3f3f3f3f;
+const ll MOD = 1e9 + 7;
+const ll N = 1e5;
+const double EPS = 1e-6;
+#define M_PI 3.14159265358979323846
+#define all(a)  a.begin(),a.end()
+#define endl '\n'
+
+int lg(int n) { return 31 - __builtin_clz(n); }
+
+const int dx[] = {+0, +0, +1, -1, +1, -1, +1, -1};
+const int dy[] = {+1, -1, +0, +0, +1, -1, -1, +1};
+int knightDx[] = {+1, -1, +2, +2, -2, -2, +1, -1};
+int knightDy[] = {+2, +2, +1, -1, +1, -1, -2, -2};
+#define int long long
+
+long long nCr(long long n, long long k) {
+    long long a = 1;
+    for (int i = 1; i <= k; i++) {
+        a = a * (n - i + 1);
+        a = a / i;
+    }
+    return a;
+}
+
+signed main() {
+    besmellah
+//Sky is the limit ❤
+#ifdef CLION
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+    //freopen("", "r", stdin);
+#endif
+    int T = 1;
+//    cin >> T;
+//remember data type long long or int
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << nCr(n - 4 + 3 - 1, 2) << endl;
+    }
+//remember data type long long or int
+
+}
